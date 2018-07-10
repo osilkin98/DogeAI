278393b24ef308a232178b12eb5bafe572d49191
Oleg Silkin <o.silkin98@gmail.com>
2018-07-09 21:56:55 -0500

    Updated twitter.py
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- twitter.py
+++ twitter.py
@@ -2,7 +2,7 @@
 import keys
 import tweepy
 from image_file import TempImage
-
+import json
 
 # set the keys in the authorization
 auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
@@ -11,27 +11,33 @@ auth.set_access_token(keys.access_token, keys.access_token_secret)
 
 # returns image object from tweet
 def get_image_from_tweet(target_tweet):
-    if 'media' in target_tweet.entities:
-        for image in target_tweet.entities['media']: # to retrieve the one element from the array
-            # For debugging
-            # print(image['media_url'])
-            return TempImage(image['media_url'])
+    for image in target_tweet.entities['media']: # to retrieve the one element from the array
+        # For debugging
+        # print(image['media_url'])
+        return TempImage(image['media_url'])
 
 
 def display_image_from_tweet(target_tweet):
-    my_image = get_image_from_tweet(target_tweet)
-    my_image.image.show()
+    if 'media' in target_tweet.entities:
+        my_image = get_image_from_tweet(target_tweet)
+        my_image.image.show()
+    else:
+        print("Tweet by {} was not an image, tweet body: {}".format(target_tweet.user.screen_name, target_tweet.text))
 
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        print("[MyStreamListener.on_status]: {}".format(status))
-
+        # print("[MyStreamListener.on_status]: {}".format(status))
+        # print("Formatting as json: ")
+        # print(json.dumps(status._json, sort_keys=True, indent=4, separators=(',', ': ')))
+        display_image_from_tweet(status)
+    ''' 
     def on_event(self, status):
         print("[MyStreamListener.on_event]: {}".format(status))
 
     def on_data(self, raw_data):
         print("[MyStreamListener.on_data]: {}".format(raw_data))
+    '''
 
 
 try:
