8ad91e733ffe8617ba97854b4456064d6da15603
Oleg Silkin <o.silkin98@gmail.com>
2018-07-10 02:10:23 -0500

    Added method to create numpy array from a given directory
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- dogedata.py
+++ dogedata.py
@@ -1,21 +1,6 @@
 import cv2
 import os
 import numpy as np
-doge_directory = '/home/oleg/Pictures/classification_data/doge/'
-doge_list = os.listdir(doge_directory)
-image_data_list = []
-for x in doge_list:
-    # print(x)
-    image_data_list.append(cv2.imread("{}{}".format(doge_directory, x)))
-print(image_data_list)
-numpy_array = np.array(image_data_list)
-print("numpy_array type: {}".format(type(numpy_array)))
-print("doge image list type: {}".format(type(image_data_list)))
-print(numpy_array.shape)
-# image = cv2.imread('/home/oleg/Pictures/doge-jpeg-jpg/28iw3x.jpg')
-# print(type(image))
-# print(image)
-
 
 # Takes a directory
 def prepare_data_from_directory(directory):
@@ -36,3 +21,7 @@ def prepare_data_from_directory(directory):
         print("[prepare_data_from_directory]: is a directory, not file: {}".format(idr.filename))
     finally:
         return np.array(to_fill)
+
+
+numpy_array = prepare_data_from_directory('/home/oleg/Pictures/classification_data/doge/')
+print(numpy_array.shape)
