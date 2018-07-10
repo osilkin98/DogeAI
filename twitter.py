a7f25941de9f375d611a0fe323d8ee95364c6230
Oleg Silkin <o.silkin98@gmail.com>
2018-07-09 21:38:35 -0500

    Added Overloads to MyStreamListener
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- twitter.py
+++ twitter.py
@@ -25,18 +25,32 @@ def display_image_from_tweet(target_tweet):
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        print(status.text)
+        print("[MyStreamListener.on_status]: {}".format(status))
+
+    def on_event(self, status):
+        print("[MyStreamListener.on_event]: {}".format(status))
+
+    def on_data(self, raw_data):
+        print("[MyStreamListener.on_data]: {}".format(raw_data))
+
 
 try:
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
     user = api.me()
 
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+    myStream.filter(track=["@doge_or"], is_async=True)
+
+    '''
     # retrieve the first post from timeline
     first_tweet = api.home_timeline()[0]
     display_image_from_tweet(first_tweet)
-
+    '''
 except tweepy.TweepError as e:
+    print(e.message)
+except Exception as e:
     print(e.message)
 finally:
     print("Program Finished Execution")
