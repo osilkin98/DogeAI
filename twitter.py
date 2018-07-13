8fd0455a2f0e1a1216bcf08854a4f8dacce90f26
Oleg Silkin <o.silkin98@gmail.com>
2018-07-12 19:20:52 -0500

    Added capability to alert users of downtime
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- twitter.py
+++ twitter.py
@@ -5,6 +5,8 @@ from image_file import TempImage
 import tensorflow as tf
 import doge_classifier as dc
 import json
+import datetime
+
 # set the keys in the authorization
 auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
@@ -12,6 +14,7 @@ try:
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 except Exception as e:
     print("Exception e raised: {}".format(e))
+
 
 # returns image object from tweet
 def get_image_from_tweet(target_tweet):
@@ -35,6 +38,7 @@ def identify_doge(image):
 
 
 def doge_or_not_doge(target_tweet):
+    print("entered doge or not doge")
     image = get_image_from_tweet(target_tweet)
     # Image was Doge
     # print(json.dumps(target_tweet.entities, indent=4, sort_keys=True, separators=(',', ': ')))
@@ -67,7 +71,19 @@ class MyStreamListener(tweepy.StreamListener):
         # print("[MyStreamListener.on_status]: {}".format(status))
         # print("Formatting as json: ")
         # print(json.dumps(status._json, sort_keys=True, indent=4, separators=(',', ': ')))
+        print("got tweet: {}".format(status.text))
         display_image_from_tweet(status)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            print("Status code 420, returning")
+            return False
+
+    def __del__(self):
+        try:
+            api.update_status("Going Down For Maintenance at {}".format(datetime.datetime.now()))
+        except tweepy.TweepError as e:
+            print("{}".format(e.response))
     ''' 
     def on_event(self, status):
         print("[MyStreamListener.on_event]: {}".format(status))
@@ -83,6 +99,7 @@ try:
 
     myStreamListener = MyStreamListener()
     myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+    print("listening")
     myStream.filter(track=["@doge_or"])
 
     '''
@@ -92,7 +109,14 @@ try:
     '''
 except tweepy.TweepError as e:
     print(e)
+except KeyboardInterrupt as ki:
+    print("{}".format(ki.args))
 except Exception as e:
     print(e)
 finally:
+    '''try:
+        api.update_status("Going Down For Maintenance at {}".format(datetime.datetime.now()))
+    except tweepy.TweepError as e:
+        print("{}".format(e.response))
+    '''
     print("Program Finished Execution")
